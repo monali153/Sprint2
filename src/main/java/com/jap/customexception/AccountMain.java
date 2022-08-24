f8ae3d9e79e41bccd909b847828f31990253baca
Monali Sharma <mona.chaudhari444@gmail.com>
2022-08-24 15:26:09 +0530

    successfully running main method and test cases also.

--- src/main/java/com/jap/customexception/AccountMain.java
+++ src/main/java/com/jap/customexception/AccountMain.java
@@ -1,4 +1,20 @@
 package com.jap.customexception;
 
 public class AccountMain {
+
+    public static void main(String[] args) {
+
+        Account account = new Account(20000);
+        Account account1 = new Account(40000);
+
+        try {
+            account1.withdraw(2000);
+            System.out.println("Account Balance = " + account1.getAccountBalance());
+            account.withdraw(30000);
+            System.out.println("Account Balance = " + account.getAccountBalance());
+        } catch (InsufficientFundException e) {
+            System.out.println(e);
+        }
+
+    }
 }
